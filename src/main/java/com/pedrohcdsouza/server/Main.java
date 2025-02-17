40474ae0a7793355bdf512a02d2c52292441abc4
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-02-17 12:05:56 -0300

    Just adding a logic to test the Db

--- src/main/java/com/pedrohcdsouza/server/Main.java
+++ src/main/java/com/pedrohcdsouza/server/Main.java
@@ -1,7 +1,10 @@
 package com.pedrohcdsouza.server;
 
+import com.pedrohcdsouza.server.database.Database;
+
 public class Main {
     public static void main(String[] args) {
-
+        Database banco = new Database();
+        banco.adicionar(0, "pedro", 0.0, 0);
     }
 }
