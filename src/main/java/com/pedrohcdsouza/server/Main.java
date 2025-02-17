40b54e284eefa7b387ed008852a66a17e18a5008
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-02-17 11:45:46 -0300

    Removing the present db

--- src/main/java/com/pedrohcdsouza/server/Main.java
+++ src/main/java/com/pedrohcdsouza/server/Main.java
@@ -1,8 +1,7 @@
 package com.pedrohcdsouza.server;
 
-import com.pedrohcdsouza.server.database.Database;
-
 public class Main {
-    Database db = new Database();
-    db.addProduct("oi", 9, 8);
+    public static void main(String[] args) {
+
+    }
 }
