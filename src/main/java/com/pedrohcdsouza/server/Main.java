6cfb9ed5bd0e9faa8eefa7c06935758a0b68eb49
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-02-17 21:23:35 -0300

    Increasing the socket/thread logic

--- src/main/java/com/pedrohcdsouza/server/Main.java
+++ src/main/java/com/pedrohcdsouza/server/Main.java
@@ -1,10 +1,11 @@
 package com.pedrohcdsouza.server;
 
 import com.pedrohcdsouza.server.database.Database;
+import com.pedrohcdsouza.server.service.ServerSocketObj;
 
 public class Main {
     public static void main(String[] args) {
-        Database banco = new Database();
-        banco.adicionar(0, "pedro", 0.0, 0);
+        ServerSocketObj sv = new ServerSocketObj(null);
+        sv.startServer();
     }
 }
--- src/main/java/com/pedrohcdsouza/server/service/ServerSocketObj.java
+++ src/main/java/com/pedrohcdsouza/server/service/ServerSocketObj.java
@@ -1,33 +1,85 @@
 package com.pedrohcdsouza.server.service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.ServerSocket;
 
-public class ServerSocketObj {
-    public void startServer(int port) {
-        System.out.println("Starting server ...");
-        ServerSocket server = null;
+import io.github.cdimascio.dotenv.Dotenv;
+
+
+
+public class ServerSocketObj extends Thread {
+
+    private static final int port = 8700;
+    private Dotenv dotenv = Dotenv.load();
+    private String authKey = dotenv.get("AUTH_KEY");
+    private Socket client;
+    private String outputResponse;
+
+    public ServerSocketObj(Socket socket) {
+        this.client = socket;
+    }
+
+    @Override
+    public void run() {
         try {
-            server = new ServerSocket(port);
-            System.out.println("Server listening on port " + port);
-            while (true) {
-                Socket client = server.accept();
-                System.out.println("Client " + client.getInetAddress().getHostAddress() + " connected.");
 
+            client.setSoTimeout(5000);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+
+            String inputResponse = in.readLine();
+
+            if (authKey == null) {
+                System.out.println("[ERROR]: AUTH_KEY not founded.");
                 client.close();
-                System.out.println("Client " + client.getInetAddress().getHostAddress() + " desconnected.");
+                return; 
             }
+
+            if (!authKey.equals(inputResponse)) {
+                System.out.println("[WARNING]: Client " + client.getInetAddress() + " Fail to Auth.");
+                client.close();
+                return; 
+            }
+
+            System.out.println("[AUTH]: Client " + client.getInetAddress() + " Authenticated.");
+
+            outputResponse = "AUTH";
+            out.println(outputResponse);
+
+            
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
+
             try {
-                if (server != null) {
-                    server.close();
+                if (client != null && !client.isClosed()) {
+                    client.close();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public void startServer() {
+        try (ServerSocket server = new ServerSocket(port)) {
+            System.out.println("[INFO]: Waiting for clients ...");
+
+            while (true) {
+                Socket client = server.accept();
+                System.out.println("[CONNECT]: Client " + client.getInetAddress() + " Connected.");
+
+                ServerSocketObj svsock = new ServerSocketObj(client);
+                svsock.start();
+            }
+        } catch (IOException e) {
+            System.out.println("[ERROR]: " + e.getMessage());
         }
     }
 }
