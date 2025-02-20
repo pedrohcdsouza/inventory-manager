3c6f2d37a97de57ffaba402b289bb4f56fe8e1cd
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-02-20 09:16:56 -0300

    Adding records to avoid unnecessarily long code

--- src/main/java/com/pedrohcdsouza/server/models/Product.java
+++ src/main/java/com/pedrohcdsouza/server/models/Product.java
@@ -1,57 +1,15 @@
 package com.pedrohcdsouza.server.models;
 
-public class Product {
-    private Integer id;
-    private String name;
-    private double price;
-    private int quantity;
-
-    public Product(Integer id, String name, double price, int quantity) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
+public record Product(Integer id, String name, double price, int quantity) {
+    public Product withName(String newName) {
+        return new Product(this.id, newName, this.price, this.quantity);
     }
-
-    public Integer getId() {
-        return id;
+    
+    public Product withPrice(double newPrice) {
+        return new Product(this.id, this.name, newPrice, this.quantity);
     }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", price=" + price +
-                ", quantity=" + quantity +
-                '}';
+    
+    public Product withQuantity(int newQuantity) {
+        return new Product(this.id, this.name, this.price, newQuantity);
     }
 }
